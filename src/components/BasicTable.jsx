dd6553be5675cdc3934cddb353ff14d68bf7750c
rksdev <mailto.dev.rakesh@gmail.com>
2022-09-18 00:19:06 +0530

    Added table and fetched data from api

---
+++ src/components/BasicTable.jsx
@@ -0,0 +1,68 @@
+import * as React from "react";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import axios from "axios";
+import { useState } from "react";
+import { useEffect } from "react";
+import EditIcon from "@mui/icons-material/Edit";
+import HighlightOffIcon from "@mui/icons-material/HighlightOff";
+
+export default function BasicTable() {
+  const [tableData, setTableData] = useState([]);
+  const handleDelete = (e) => {};
+
+  useEffect(() => {
+    axios
+      .get("https://api.escuelajs.co/api/v1/products")
+      .then((res) => {
+        console.log(res.data);
+        setTableData(res.data.splice(1, 20));
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  return (
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>ID</TableCell>
+            <TableCell align="center">Title</TableCell>
+            <TableCell align="center">Description</TableCell>
+            <TableCell align="center">Category</TableCell>
+            <TableCell align="center">Price</TableCell>
+            <TableCell align="center">Edit</TableCell>
+            <TableCell align="center">Delete</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {tableData.map((row) => (
+            <TableRow
+              key={row.id}
+              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                {row.id}
+              </TableCell>
+              <TableCell align="left">{row.title}</TableCell>
+              <TableCell align="left">{row.description}</TableCell>
+              <TableCell align="left">{row.category.name}</TableCell>
+              <TableCell align="left">{row.price}</TableCell>
+              <TableCell>
+                <EditIcon />
+              </TableCell>
+              <TableCell>
+                <HighlightOffIcon onClick={handleDelete} />
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,60 @@
+import Switch from "@mui/material/Switch";
+import styled from "styled-components";
+import BasicTable from "../components/BasicTable";
+
+const MainContainer = styled.div`
+  display: flex;
+  flex-direction column;
+  justify-content: center;
+  align-items: center;
+  max-width: 1024px;
+  margin: auto;
+  background-color: #eeeeee;
+`;
+const HeadingContainer = styled.div``;
+const Heading = styled.h1``;
+
+const SearchContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  justify-content: space-evenly;
+`;
+const SearchInputDiv = styled.div``;
+
+const SelectInputDiv = styled.div``;
+const ToggleInputDiv = styled.div``;
+const TableContainer = styled.div``;
+
 const Home = () => {
-  return <div>Home</div>;
+  const label = { inputProps: { "aria-label": "Switch demo" } };
+  return (
+    <MainContainer>
+      <HeadingContainer>
+        <Heading>Platzi Table</Heading>
+      </HeadingContainer>
+      <SearchContainer>
+        <SearchInputDiv>
+          <input type="text" placeholder="search" />
+        </SearchInputDiv>
+        <SelectInputDiv>
+          <select id="cars" name="cars">
+            <option value="id">Id</option>
+            <option value="title">Title</option>
+            <option value="price">Price Title</option>
+            <option value="description">Description</option>
+            <option value="category">Category Name</option>
+          </select>
+        </SelectInputDiv>
+        <ToggleInputDiv>
+          <Switch {...label} />
+        </ToggleInputDiv>
+      </SearchContainer>
+      <TableContainer>
+        <BasicTable />
+      </TableContainer>
+    </MainContainer>
+  );
 };
 export default Home;
